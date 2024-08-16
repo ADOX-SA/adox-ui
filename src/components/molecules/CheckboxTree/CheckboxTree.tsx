aaf76e818bdb0f95948e91d9dea60fb063d84118
albertoleandrosanchez <albertoleandrosanchez@hotmail.com>
2024-08-16 12:29:49 -0300

    refactor: Add CheckboxTree component
    The CheckboxTree component has been added to the project. This component allows users to select multiple options from a hierarchical tree structure using checkboxes. It provides a visually appealing and user-friendly way to handle complex data selection. This addition enhances the functionality and usability of the application.

---
+++ src/components/molecules/CheckboxTree/CheckboxTree.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import styles from "./CheckboxTree.module.css";
+
+export type CheckboxTreeProps = {
+  // types...
+};
+
+const CheckboxTree: React.FC<CheckboxTreeProps> = ({}) => {
+  return <div className={styles.checkboxtree}>a</div>;
+};
+
+export default CheckboxTree;
